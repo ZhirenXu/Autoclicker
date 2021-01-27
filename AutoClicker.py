391b1f9658d385c2346cc509e3f8eac006fe3403
ZhirenXu <xu.3579@buckeyemail.osu.edu>
2021-01-26 20:36:37 -0500

    test for seleniummmmmmmmmmmmmmmmmmmmmm

--- AutoClicker.py
+++ AutoClicker.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from Function import Login
+from selenium.webdriver.common.by import By
 
 def installDriver():
-
+    pass
 def demo():
     driver = webdriver.Firefox()
     driver.get("http://www.python.org")
@@ -13,3 +15,23 @@ def demo():
     elem.send_keys(Keys.RETURN)
     assert "No results found." not in driver.page_source
     driver.close()
+
+def test():
+    #browser = Login.login()
+    #creditCookie = browser.session.cookies.get_dict()
+    #keyList = list(creditCookie.keys())
+    #key = keyList[0]
+    #print(creditCookie)
+    #get website
+    driver = webdriver.Firefox()
+    #cookie = {'name': key, 'value': creditCookie[key]}
+    #driver.add_cookie(cookie)
+    driver.get("https://library.osu.edu/dc/concern/generic_works/s1784n301?locale=en")
+    driver.find_element_by_xpath('//button[@type="button"]/span["Login"]').click()
+    #driver.find_element_by_link_text("https://library.osu.edu/dc/users/auth/shibboleth?locale=en").click()
+    #driver.find_element_by_id("File Manager").click()
+def main(*argv):
+    test()
+    
+if __name__ == "__main__":
+    main()
